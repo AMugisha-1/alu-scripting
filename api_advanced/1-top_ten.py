617dd8cc32247f2d463911116693b9a9a9cfc4c8
AMugisha-1 <a.mugisha1@alustudent.com>
2025-02-17 03:22:27 -0800

    Reddit c

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,13 +4,21 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """ Prints the titles of the first 10 hot posts listed in a subreddit """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    try:
+        posts = response.json().get("data", {}).get("children", [])
+        if not posts:
+            print(None)
+        else:
+            for post in posts:
+                print(post["data"].get("title", "None"))
+    except Exception:
+        print(None)
