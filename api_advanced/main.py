e88bbfbb39123660b3f00df9244cacdcdb5ebe24
AMugisha-1 <a.mugisha1@alustudent.com>
2025-02-21 02:34:10 -0800

    Call the file for//tests---

--- api_advanced/main.py
+++ api_advanced/main.py
@@ -2,11 +2,11 @@
 """ 1-main.py """
 
 import sys
-from 1-top_ten import top_ten  # Importing your function
+from 1-top_ten import 1-top_ten  # Importing your function
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        top_ten(sys.argv[1])
+       1-top_ten(sys.argv[1])
 
