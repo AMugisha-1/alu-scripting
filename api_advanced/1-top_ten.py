27f7b5b32fe188e2babcef1584e3b70a55c709dd
AMugisha-1 <a.mugisha1@alustudent.com>
2025-02-21 12:06:08 -0800

    GAMAA-A

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
+"""Module to print the titles of the first 10 hot posts of a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code != 200:
-        print("OK")
+        print("OK", end="")  # Ensures no extra newline character
         return
-    posts = response.json()['data']['children']
+
+    posts = response.json().get('data', {}).get('children', [])
     for post in posts:
-        print(post['data']['title'])
+        print(post['data'].get('title', ""))
