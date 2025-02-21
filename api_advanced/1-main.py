2aa1bb8caf6f4aeb6c9b51cd5c46f4c1e012c616
AMugisha-1 <a.mugisha1@alustudent.com>
2025-02-21 02:20:26 -0800

    Call the file for tests

---
+++ api_advanced/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 1-main.py """
+
+import sys
+from 1-top_ten import top_ten  # Importing your function
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
+
