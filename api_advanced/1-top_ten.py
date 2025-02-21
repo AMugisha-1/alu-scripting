afb22f03963f88a4cfc470d2313ab96787c0c5db
AMugisha-1 <a.mugisha1@alustudent.com>
2025-02-21 11:14:58 -0800

    TOP MM

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,13 +4,20 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """ Prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
-        print(None)
+        print("OK")  # Expected output for non-existent subreddit
         return
-    posts = response.json()['data']['children']
+
+    posts = response.json().get('data', {}).get('children', [])
+
+    if not posts:
+        print("OK")  # Expected output for an empty subreddit
+        return
+
     for post in posts:
         print(post['data']['title'])
